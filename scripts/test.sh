1446ac6e1784292658347bd1f47bfa0ac691ebbb
JFCCoding12 <jakefolsom12@gmail.com>
2017-10-09 16:07:13 -0400

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,11 @@
-sleep 10
+# Terminal Colors
+red=$'\e[1;31m'
+grn=$'\e[1;32m'
+yel=$'\e[1;33m'
+blu=$'\e[1;34m'
+mag=$'\e[1;35m'
+cyn=$'\e[1;36m'
+end=$'\e[0m'
 
-printf "\e[COLORm sample text\n"
+
+printf "%s\n${blu}Installing native dependencies${end}\n"
